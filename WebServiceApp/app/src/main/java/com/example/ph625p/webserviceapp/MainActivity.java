4a0396a93b701c63edcfa2728d72a1b9340d2d3d
Bradley Monroe <monroe0219@gmail.com>
2018-08-01 17:20:57 -0500

    API modifications

--- WebServiceApp/app/src/main/java/com/example/ph625p/webserviceapp/MainActivity.java
+++ WebServiceApp/app/src/main/java/com/example/ph625p/webserviceapp/MainActivity.java
@@ -20,9 +20,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.dataview = (TextView)findViewById(R.id.data);
-        //this.webView = (WebView)findViewById(R.id.webdata);
+        this.webView = (WebView)findViewById(R.id.webdata);
         //WebView webView = new WebView(this);
-        setContentView(webView);
+       // setContentView(webView);
 
     }
 
@@ -33,19 +33,24 @@ public class MainActivity extends AppCompatActivity {
 //        APIRequest request = new APIRequest(getApplicationContext(),"http://www.mocky.io/v2/5b5bb2663200004300426251","POST");
 //        request.postData.add(new Pair<String, String>("email","pettish@frootfel.com"));
 //        request.headerData.add(new Pair<String, String>("Token","djaieslhttieoshoto"));
-        APIRequest request = new APIRequest(getApplicationContext(),"https://account.box.com/api/oauth2/authorize","POST");
+/*        APIRequest request = new APIRequest(getApplicationContext(),"https://account.box.com/api/oauth2/authorize","POST");
         request.postData.add(new Pair<String, String>("response_type","code"));
         request.postData.add(new Pair<String, String>("client_id","kaclfbxuw6r4i4oixp3btszww6rp822y"));
         request.postData.add(new Pair<String, String>("redirect_uri","https://app.box.com"));
         request.postData.add(new Pair<String, String>("state","bradley"));
+*/      APIRequest request = new APIRequest(getApplicationContext(),"https://api.spotify.com/v1/me/player/next","POST");
+        request.postData.add(new Pair<String, String>("device_id","0d1841b0976bae2a3a310dd74c0f3df354899bc8"));
+        request.headerData.add(new Pair<String, String>("Accept:", "application/json"));
+        request.headerData.add(new Pair<String, String>("Content-Type:","application/json"));
+        request.headerData.add(new Pair<String, String>("Authorization:","Bearer BQCrr_gJOgnqQS0UtWvbCz7QdxFwcbsxr4zuj68YhGeEw6q_Ti3A93EniGfNYxP9eN6jXqYh6mo6nilHxLBh6DM-LbXbqQlN_KEHWVjKa5n5XidgsK5JFRFQyFSwiphFlMptBtSxquQVW6kA7jUqr9yDRQ"));
         APIDataTask apiDataTask = (APIDataTask) new APIDataTask(request, new APIDataTask.OnCompleteListener(){
 
             @Override
             public void onComplete(APIResponse result) {
                 Log.d("Response",result.responseData);
                 dataview.setText(result.responseData);
-                webView.loadUrl(result.responseData);
-                webView.loadDataWithBaseURL("", result.responseData, "text/html", "UTF-8", "");
+     //           webView.loadUrl(result.responseData);
+                webView.loadDataWithBaseURL("", result.responseData, "application/json", "UTF-8", "");
 
             }
         }).execute(request);
