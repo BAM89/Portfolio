eeae22e7c3f4574f2179353061700dbd42b84a28
Bradley Monroe <monroe0219@gmail.com>
2018-08-03 17:32:42 -0500

    Callback modifications

--- WebServiceApp/app/src/main/java/com/example/ph625p/webserviceapp/MainActivity.java
+++ WebServiceApp/app/src/main/java/com/example/ph625p/webserviceapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.ph625p.webserviceapp;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -33,16 +34,17 @@ public class MainActivity extends AppCompatActivity {
 //        APIRequest request = new APIRequest(getApplicationContext(),"http://www.mocky.io/v2/5b5bb2663200004300426251","POST");
 //        request.postData.add(new Pair<String, String>("email","pettish@frootfel.com"));
 //        request.headerData.add(new Pair<String, String>("Token","djaieslhttieoshoto"));
-/*        APIRequest request = new APIRequest(getApplicationContext(),"https://account.box.com/api/oauth2/authorize","POST");
+        APIRequest request = new APIRequest(getApplicationContext(),"https://account.box.com/api/oauth2/authorize","POST");
         request.postData.add(new Pair<String, String>("response_type","code"));
         request.postData.add(new Pair<String, String>("client_id","kaclfbxuw6r4i4oixp3btszww6rp822y"));
-        request.postData.add(new Pair<String, String>("redirect_uri","https://app.box.com"));
+        request.postData.add(new Pair<String, String>("redirect_uri","appSchema://webserviceapp.com"));
         request.postData.add(new Pair<String, String>("state","bradley"));
-*/      APIRequest request = new APIRequest(getApplicationContext(),"https://api.spotify.com/v1/me/player/next","POST");
+  /*    APIRequest request = new APIRequest(getApplicationContext(),"https://api.spotify.com/v1/me/player/next","POST");
         request.postData.add(new Pair<String, String>("device_id","0d1841b0976bae2a3a310dd74c0f3df354899bc8"));
         request.headerData.add(new Pair<String, String>("Accept:", "application/json"));
         request.headerData.add(new Pair<String, String>("Content-Type:","application/json"));
         request.headerData.add(new Pair<String, String>("Authorization:","Bearer BQCrr_gJOgnqQS0UtWvbCz7QdxFwcbsxr4zuj68YhGeEw6q_Ti3A93EniGfNYxP9eN6jXqYh6mo6nilHxLBh6DM-LbXbqQlN_KEHWVjKa5n5XidgsK5JFRFQyFSwiphFlMptBtSxquQVW6kA7jUqr9yDRQ"));
+       */
         APIDataTask apiDataTask = (APIDataTask) new APIDataTask(request, new APIDataTask.OnCompleteListener(){
 
             @Override
@@ -65,5 +67,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        String CALLBACK_URL = getResources().getString(R.string.box_callback);
+        Uri uri = getIntent().getData();
+        if (uri != null && uri.toString().startsWith(CALLBACK_URL)) {
+            String access_token = uri.getQueryParameter("code");
+        }
     }
 }
